2faa181e5b73f63f4083216b6b3cf644566879f3
Ignacio Armella <gabrielarmella.70@gmail.com>
2025-01-26 22:31:29 -0300

    crea las funcionalidades al archivo app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    const valueInput = document.getElementById("amigo").value;
+    if (valueInput == "") {
+        alert("Por favor, ingresa el nombre del amigo");        
+    } else {
+        amigos.push(valueInput);
+        limpiarInput()
+    }
+    recorrerAmigos();
+    
+}
+
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+}
+
+function recorrerAmigos() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    for(let i = 0; i < amigos.length; i++){
+        const item = document.createElement("LI");
+        item.textContent = amigos[i];
+        listaAmigos.appendChild(item);
+    };
+};
+
+function sortearAmigo() {
+    //verificar si el array amigos esta vacio
+    if (amigos.length == 0) {
+        alert("No hay amigos para ordenar");
+    }else {
+        //generar indice aleatorio
+        let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = document.getElementById("resultado");
+        amigoSorteado.textContent = amigos[indiceAleatorio];
+    };
+};
